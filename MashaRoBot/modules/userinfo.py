5da9821bae885e656b5cddb03bcee52dbed59ee5
thundertech9 <81930771+thundertech9@users.noreply.github.com>
2021-06-02 17:36:15 +0530

    Update userinfo.py

--- MashaRoBot/modules/userinfo.py
+++ MashaRoBot/modules/userinfo.py
@@ -241,18 +241,18 @@ def info(update: Update, context: CallbackContext):
     rep = message.reply_text("<code>Appraising...</code>", parse_mode=ParseMode.HTML)
 
     text = (
-        f"╒═══「<b>⭐Appraisal results⭐:</b> 」\n"
+        f"╒══「<b>⭐Appraisal results⭐:</b> 」\n"
         f"📝ID: <code>{user.id}</code>\n"
-        f"✨First Name: {html.escape(user.first_name)}"
+        f"🌟First Name: {html.escape(user.first_name)}"
     )
 
     if user.last_name:
-        text += f"\n❗Last Name: {html.escape(user.last_name)}"
+        text += f"\n✨Last Name: {html.escape(user.last_name)}"
 
     if user.username:
-        text += f"\n❗Username: @{html.escape(user.username)}"
+        text += f"\n✨Username: @{html.escape(user.username)}"
 
-    text += f"\n⭕Permalink: {mention_html(user.id, 'link')}"
+    text += f"\n✨Permalink: {mention_html(user.id, 'link')}"
 
     if chat.type != "private" and user_id != bot.id:
         _stext = "\nPresence: <code>{}</code>"
